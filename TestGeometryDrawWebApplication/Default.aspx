9bedd1b3a43e387c64094bef028cd9c202182cc3
George Pavlovych <pavlheo@hotmail.com>
2016-04-19 22:27:33 +0300

    ASP.NET image is made responsive (clicking on it displays needed info)

--- TestGeometryDrawWebApplication/Default.aspx
+++ TestGeometryDrawWebApplication/Default.aspx
@@ -1,7 +1,20 @@
 ﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TestGeometryDrawWebApplication._Default" %>
-
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-
-    <img width="741" height="856" src="DrawMap.ashx?width=741&height=856"/>
-
+    <script type="text/javascript">
+        $(document).ready(function()
+        {
+            $('img').click(function(e)
+            {
+                var offset = $(this).offset();
+                var x = e.pageX - offset.left;
+                var y = e.pageY - offset.top;
+                $.ajax("DrawMap.ashx?mode=getinfo&x=" + x + "&y=" + y + "&width=741&height=856")
+                    .success(function(result)
+                    {
+                        alert(result);
+                    });
+            });
+        });
+    </script>
+    <img width="741" height="856" src="DrawMap.ashx?mode=draw&width=741&height=856"/>
 </asp:Content>
--- TestGeometryDrawWebApplication/DrawMap.ashx.cs
+++ TestGeometryDrawWebApplication/DrawMap.ashx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
+using System.Text;
 using System.Web;
 using MapVizualizer;
 
@@ -26,39 +27,78 @@ namespace TestGeometryDrawWebApplication
                                  { ColorTranslator.FromHtml("#E15151"), "<30%" },
                                  { ColorTranslator.FromHtml("#BFBFBF"), "Onbekend" }
                              };
-            var font = new Font(FontFamily.GenericMonospace, 8, FontStyle.Bold);
-            var legendFontColor = Color.Black;
-            var widthStr = context.Request[ "width" ] ?? "";
-            var heightStr = context.Request[ "height" ] ?? "";
-
+            var widthStr = context.Request["width"] ?? "";
+            var heightStr = context.Request["height"] ?? "";
             int width;
             int height;
             if (!int.TryParse(widthStr, out width))
                 width = 500;
             if (!int.TryParse(heightStr, out height))
                 height = 500;
-            using (
-                var result = MapHelper.DrawMap(
-                    new Size(width, height),
-                    50,
-                    50,
-                    50,
-                    50,
-                    Color.White,
-                    new Pen(Color.Black, 1.0f),
-                    legend,
-                    legendCaption,
-                    font,
-                    legendFontColor,
-                    1.5f))
+            var mode = context.Request[ "mode" ] ?? "";
+            if (mode == "draw")
             {
-                // set MIME type
-                context.Response.ContentType = "image/png";
+                var font = new Font(FontFamily.GenericMonospace, 8, FontStyle.Bold);
+                var legendFontColor = Color.Black;
+                using (
+                    var result = MapHelper.DrawMap(
+                        new Size(width, height),
+                        50,
+                        50,
+                        50,
+                        50,
+                        Color.White,
+                        new Pen(Color.Black, 1.0f),
+                        legend,
+                        legendCaption,
+                        font,
+                        legendFontColor,
+                        1.5f))
+                {
+                    // set MIME type
+                    context.Response.ContentType = "image/png";
 
-                // write to response stream
-                result.Save(context.Response.OutputStream, ImageFormat.Png);
+                    // write to response stream
+                    result.Save(context.Response.OutputStream, ImageFormat.Png);
+                }
             }
+            else if (mode == "getinfo")
+            {
 
+                var xStr = context.Request["x"] ?? "";
+                var yStr = context.Request["y"] ?? "";
+                float x;
+                float y;
+                if (!float.TryParse(xStr, out x))
+                    x = 0;
+                if (!float.TryParse(yStr, out y))
+                    y = 0;
+                var city = MapHelper.GetCityAtPoint(
+               new Size(width, height),
+               50,
+               50,
+               50,
+               50,
+               new PointF(x, y));
+                if (city != null)
+                {
+                    var stringBuilder = new StringBuilder();
+                    stringBuilder.AppendFormat("City Name: {0}", city.CityName);
+                    stringBuilder.AppendLine();
+                    stringBuilder.AppendFormat("Value: {0}", legend[city.CityColor]);
+                    {
+                        // set MIME type
+                        context.Response.ContentType = "text/plain";
+
+                        // write to response stream
+                        context.Response.Write(stringBuilder.ToString());
+                    }
+                }
+                else
+                {
+                    context.Response.StatusCode = 404;
+                }
+            }
         }
 
         public bool IsReusable
